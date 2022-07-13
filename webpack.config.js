6a0cefd9c2c350de8b9fbb4665e3f2e8c3b3a271
firdavsnurpaizov <firdavs.nurpaizov.ss@gmail.com>
2022-07-13 13:46:32 +0600

    init project

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    mode: 'development',
+    entry: ['@babel/polyfill', './index.js']
+    ,
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.json'],
+        alias: {
+            '@models': path.resolve(__dirname, 'src/models'),
+            '@': path.resolve(__dirname, 'src')
+        },
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: "./index.html", filename: "index.html"
+        }),
+        new HTMLWebpackPlugin({
+            template: "./pushkin.html", filename: "pushkin.html"
+        }),
+        new HTMLWebpackPlugin({
+            template: "./hachapuri.html", filename: "hachapuri.html"
+        }),
+        new HTMLWebpackPlugin({
+            template: "./samurai.html", filename: "samurai.html"
+        }),
+        new CleanWebpackPlugin(),
+        new CopyWebpackPlugin({
+            patterns: [
+              {
+                from: path.resolve(__dirname, "./src/assets"),
+                to: path.resolve(__dirname, "dist/assets"),
+              },
+            ],
+          }),
+    ],
+    devServer: {
+        port: 4200,     
+        static: {
+            directory: path.join(__dirname, 'src'),
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(sass|scss)$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/,
+                type: "asset/resource",
+            }
+        ]
+    }
+}
